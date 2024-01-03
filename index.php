9d8dcb1f564cfde5df266f1140bcf9a62030daa7
Luis <luismaja.work@gmail.com>
2024-01-03 11:38:09 +0100

    Add array of currency exchange rates

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$exchangeRates = array(
+    "EUR" => array(
+        "JPY" => 156,08
+        "KRW" => 1431,19
+    ),
+    "JPY" => array(
+        "EUR" => 0,0064
+        "KRW" => 9,17
+    ),
+    "KRW" => array(
+        "EUR" => 0,00070
+        "JPY" => 0,11
+    )
+
+)
+?>
