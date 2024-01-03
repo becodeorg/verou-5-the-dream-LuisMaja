1b488cdd5eb837e76b52c6babc765e8e60feb8dd
Luis <luismaja.work@gmail.com>
2024-01-03 15:52:00 +0100

    make the select buttons with currency options

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 $exchangeRates = array(
     "EUR" => array(
-        "JPY" => 156,08
-        "KRW" => 1431,19
+        "JPY" => 156.08,
+        "KRW" => 1431.19,
     ),
     "JPY" => array(
-        "EUR" => 0,0064
-        "KRW" => 9,17
+        "EUR" => 0.0064,
+        "KRW" => 9.17,
     ),
     "KRW" => array(
-        "EUR" => 0,00070
-        "JPY" => 0,11
-    )
-
-)
+        "EUR" => 0.00070,
+        "JPY" => 0.11,
+    ),
+);
+echo $exchangeRates;
 ?>
